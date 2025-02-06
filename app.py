92807db304fbd5eb4aa2ac49d03dfea74738bf95
Emerson De La Cruz <92970783+Mdlc33@users.noreply.github.com>
2025-02-06 09:37:49 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+# Cargar el modelo 
+model = load_model('/content/drive/MyDrive/Colab Notebooks/Redes Neuronales/models/fashion_mnist.keras')
+
+
+# Crear intefaz de usuario
+
+st.title('Clasificador de ropa')
+st.write('Sube una imagen para clasificaral como una categoría de ropa.')
+#subir imagen
+uploaded_file = st.file_uploader("Sube una imagen en escala de grises de 28x28 píxeles.", type=["jpg", "jpeg", "png"])
+
+
+if uploaded_file is not None:
+  #Vamos a procesar la imagen
+  image=Image.open(uploaded_file).convert('L') #con convert('L') se convierte de rgb a blanco y negro
+  image=image.resize((28,28)) # con esto redimencionamos la imagen
+  img_array=np.array(image) # con  numpy convertimos la iamgen en un array para posterirormente normalizarla
+  img_array=img_array/255.0 # normalizamos la imagen
+
+  # la primera posici indiaca que solo hay una imagen los siguietes dos las dimesciones de la imagen y 
+  # el ultimo digito que solo hay un canal de color (blanco y negro)
+  img_array = img_array.reshape(1,28,28,1)
+
+  # Mostrar la imagen subida
+
+  st.image(image,caption='Imagen subida',use_column_width=True)
+
+  # prediccion
+  prediction=model.predict(img_array)
+
+  classes = ['Camiseta/top', 'Pantalón', 'Jersey', 'Vestido', 'Abrigo', 'Sandalia', 'Camisa', 'Zapatilla', 'Bolso', 'Bota']
+
+  st.write('Predicción:', classes[np.argmax(prediction)])
